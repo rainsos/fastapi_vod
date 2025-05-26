d414b6ba5075a55d24a6c3a5d2a77080b9c0c756
Chansman <rainsos@nate.com>
2025-05-26 10:24:17 +0900

    Initial mypy

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+from typing import Dict
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> Dict[str, str]:
     return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
-async def say_hello(name: str):
+async def say_hello(name: str) -> Dict[str, str]:
     return {"message": f"Hello {name}"}
